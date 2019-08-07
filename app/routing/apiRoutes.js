e5d37e235c176caad216c29f0ea545cf252f8cae
shuh <nonya@biz.com>
2019-08-07 14:47:09 -0400

    first part works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,32 +3,32 @@ let bestMatch = {};
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
-    });
+    })};
 
-    app.post("/api/friends", function (req, res) {
-        const newFriend = req.body;
-        let points = 0;
-        let score1 = 0;
-        let score2 = 0;
-        let highScore = 0;
+    // app.post("/api/friends", function (req, res) {
+    //     const newFriend = req.body;
+    //     let points = 0;
+    //     let score1 = 0;
+    //     let score2 = 0;
+    //     let highScore = 0;
 
-        friendsData.forEach(element => {
-            for (let i = 0; i < element.scores.length; i++) {
-                if (element.scores[i] === newFriend.scores[i]) {
-                    points++;
-                }
-                score1 = points;
-                if (score1 > score2) {
-                    bestMatch = element;
-                }
-                score2 = score1;
-            }
-        });
+        // friendsData.forEach(element => {
+        //     for (let i = 0; i < element.scores.length; i++) {
+        //         if (element.scores[i] === newFriend.scores[i]) {
+        //             points++;
+        //         }
+        //         score1 = points;
+        //         if (score1 > score2) {
+        //             bestMatch = element;
+        //         }
+        //         score2 = score1;
+        //     }
+        // });
+    // });
 
-        friendsData.push(req.body);
-        res.sendStatus(200);
+        // friendsData.push(req.body);
+        // res.sendStatus(200);
 
     //     $.post("/api/friends"), data, function(results)
     //     {$("#bf-name").text(results.fullName)
     // }
-
